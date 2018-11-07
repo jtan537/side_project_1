737d848fc4580ce7f74f1c42f020a33cd9f673ae
Jimmy <jtan5372@gmail.com>
2018-11-06 21:17:59 -0300

    completed basic site scraper

--- SideProject1/app/src/main/java/com/example/jimmy/sideproject1/textScraper.java
+++ SideProject1/app/src/main/java/com/example/jimmy/sideproject1/textScraper.java
@@ -1,47 +1,40 @@
 package com.example.jimmy.sideproject1;
 
-import java.util.List;
-import java.util.ArrayList;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+        import java.io.IOException;
+        import java.util.List;
+        import java.util.ArrayList;
+        import org.jsoup.Jsoup;
+        import org.jsoup.nodes.Document;
+        import org.jsoup.nodes.Element;
+        import org.jsoup.select.Elements;
 
-public class textScraper {
-    /**
-     * The current year of the time table
-     */
-    private String CURRENTYEAR = "2018";
+public final class textScraper {
 
-    /**
-     * The course code to scrape information for.
-     */
-    private String courseCode;
+    private static String generateCurYearSuffix(String courseCode) {
+        // If it's a winter course the end year suffix of URL changes.
+        if (courseCode.charAt(courseCode.length() - 1) == 'S') {
+            return "20191";
+        } else {
+            return "20189";
+        }
+    }
 
-    /**
-     * The list of relevant information.
-     */
-
-
-    public static void main(String[] args) throws Exception {
-        String url = "http://coursefinder.utoronto.ca/course-search/search/courseInquiry?methodToCall=start&viewId=CourseDetails-InquiryView&courseId=CSC148H1F20189";
+    public List getCourseInfo(String courseCode) throws IOException {
+        String currentYear = generateCurYearSuffix(courseCode);
+        String url = "http://coursefinder.utoronto.ca/course-search/search/courseInquiry?methodToCall=start&viewId=CourseDetails-InquiryView&courseId=" + courseCode + currentYear;
         Document document = Jsoup.connect(url).get();
-        Elements colspan = document.getElementsByAttributeValue("colspan","1");
+        Elements colspan = document.getElementsByAttributeValue("colspan", "1");
         Elements uif = document.getElementsByAttributeValue("class", "uif-field");
         Elements total = new Elements();
         List<String> text = new ArrayList<>();
-        for(Element e: colspan){
-            for(Element j: uif){
-                if(e.hasSameValue(j)){
+        for (Element e : colspan) {
+            for (Element j : uif) {
+                if (e.hasSameValue(j)) {
                     total.add(e);
                     text.add(e.text());
                 }
             }
-
         }
-
-
-        System.out.println(text);
+        return text;
     }
-
 }
