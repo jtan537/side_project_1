cbb00f9d1764072a7d6b8169dcb1062d5679d552
Jimmy <jtan5372@gmail.com>
2018-11-08 16:21:49 -0300

    made it public

--- SideProject1/app/src/main/java/com/example/jimmy/sideproject1/TimetableActivity.java
+++ SideProject1/app/src/main/java/com/example/jimmy/sideproject1/TimetableActivity.java
@@ -1,14 +1,26 @@
 package com.example.jimmy.sideproject1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TimetableActivity extends AppCompatActivity {
 
     private static String[] courseCodes = new String[6];
+
+    private static List<TextView> timeSlots = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
+    }
+
+    private void initializeTimeSlots(Context context) {
+        this.timeSlots.clear();
+
     }
 }
