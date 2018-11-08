8a9defe4c8ea88b4135fd9bc94d99f6002ace621
Jimmy <jtan5372@gmail.com>
2018-11-08 15:46:44 -0300

    Modified infoslicer to implement the daily class, and return a list of daily classes.

--- SideProject1/app/src/main/java/com/example/jimmy/sideproject1/infoSlicer.java
+++ SideProject1/app/src/main/java/com/example/jimmy/sideproject1/infoSlicer.java
@@ -98,70 +98,96 @@ public final class infoSlicer {
     }
 
     /**
-     * Return a list of given course info, with the data/time info turned into a hash-map.
-     *
-     * PRECONDITION:     courseInfo is a list returned by getCourseInfo.
-     * @param courseInfo a list of all the course info, scraped on the U of T coursefinder site.
-     * @return           A 2-d list where the date/time is hash-mapped to the day and a array of
-     *                   start times.
+     * Returns a list of DailyClasses, given a course code.
+     * @param courseCode The course code to generate classes from the U of T coursefinder site.
+     * @return           The list of DailyClasses.
+     * @throws IOException
      */
-    private static List<List> mapDateTime(List<String> courseInfo){
-        List<List<String>> slicedCourseInfo = sliceCourseInfo(courseInfo);
-        // Take all values of sliced course info, and change the date time to a hashmap.
-        List<List> mappedDateTime = new ArrayList();
-        for(List lst: slicedCourseInfo){
-            // The hash-map according to the lecture class.
-            Map<String, Time> curMappedDateTime = new HashMap<>();
-            // Get the current grouped date time, from lst.
-            List<List<String>> curGroupedDateTime = groupBrokenString(breakUpDateTime((String)lst.get(1)), 3);
-            for(List group: curGroupedDateTime){
-                // Initialize current values.
-                String curDay = "";
-
-                // Set the start/end time to the corresponding grouped value, casted as an int
-                int startTime = Integer.valueOf((String)group.get(1));
-                int endTime = Integer.valueOf((String)group.get(2));
-                Time curTime = new Time(startTime, endTime);
-                curDay = (String)group.get(0);
-
-                // Add to current hashmap
-                curMappedDateTime.put(curDay, curTime);
-            }
-            // After finished the hashmap for the current lecture section, set the sliced info's
-            // date time index to the hashmap, which is always index 1 according U of T format.
-            lst.set(1, curMappedDateTime);
-            mappedDateTime.add(lst);
-        }
-        return mappedDateTime;
-    }
-
-    /**
-     * Returns a list of lectures, given a course.
-     * PRECONDITION:     courseInfo is a list returned by getCourseInfo.
-     * @param courseInfo a list of all the course info, scraped on the U of T coursefinder site.
-     * @return           a list of lecture objects given a course.
-     */
-    private static List<Lecture> instantiateLectures(List<String> courseInfo){
-        List<List> mappedCourseInfo = mapDateTime(courseInfo);
-        List<Lecture> lectures = new ArrayList<>();
-        for(List lst: mappedCourseInfo){
-            // Initialize the lecture with the lecture string(LEC 5101) and the time object.
-            // Can modify lecture to take in additional info, such as
-            Lecture curLecture = new Lecture((String)lst.get(0), (HashMap)lst.get(1));
-            lectures.add(curLecture);
-        }
-        return lectures;
-    }
-
-    /**
-     * Instantiates a course, given a courseCode String.
-     * @param courseCode The course to instantiate.
-     * @return           A course containing a course code and list of Lectures.
-     */
-    public static Course instantiateCourse(String courseCode) throws IOException {
+    private static List<DailyClass> instantiateDailyClasses(String courseCode) throws IOException{
         List<String> courseInfo = textScraper.getCourseInfo(courseCode);
-        List<Lecture> lectures = instantiateLectures(courseInfo);
-        Course newCourse = new Course(courseCode, lectures);
-        return newCourse;
+        List<List<String>> slicedCourseInfo = sliceCourseInfo(courseInfo);
+        List<DailyClass> dailyClasses= new ArrayList<>();
+        for(List lecture: slicedCourseInfo){
+            // Initialize the information to create classes from
+            String curLectureSection = (String)lecture.get(0);
+            List<List<String>> curGroupedDateTime = groupBrokenString(breakUpDateTime((String)lecture.get(1)), 3);
+            for(List day: curGroupedDateTime) {
+                //Initialize the daily class for this particular day, where day = (Day, start, end)
+                DailyClass curDailyClass = new DailyClass((String) day.get(0), courseCode,
+                        curLectureSection, (int) day.get(1), (int) day.get(2));
+                dailyClasses.add(curDailyClass);
+            }
+        }
+        return dailyClasses;
     }
+
+//    /**
+//     * Return a list of given course info, with the data/time info turned into a hash-map.
+//     *
+//     * PRECONDITION:     courseInfo is a list returned by getCourseInfo.
+//     * @param courseInfo a list of all the course info, scraped on the U of T coursefinder site.
+//     * @return           A 2-d list where the date/time is hash-mapped to the day and a array of
+//     *                   start times.
+//     */
+//    private static List<List> mapDateTime(List<String> courseInfo){
+//        List<List<String>> slicedCourseInfo = sliceCourseInfo(courseInfo);
+//        // Take all values of sliced course info, and change the date time to a hashmap.
+//        List<List> mappedDateTime = new ArrayList();
+//        for(List lst: slicedCourseInfo){
+//            // The hash-map according to the lecture class.
+//            Map<String, DailyClass> curMappedDateTime = new HashMap<>();
+//            // Get the current grouped date time, from lst.
+//            List<List<String>> curGroupedDateTime = groupBrokenString(breakUpDateTime((String)lst.get(1)), 3);
+//            for(List group: curGroupedDateTime){
+//                // Initialize current values.
+//                String curDay = "";
+//
+//                // Set the start/end time to the corresponding grouped value, casted as an int
+//                int startTime = Integer.valueOf((String)group.get(1));
+//                int endTime = Integer.valueOf((String)group.get(2));
+//                DailyClass curTime = new DailyClass(startTime, endTime);
+//                curDay = (String)group.get(0);
+//
+//                // Add to current hashmap
+//                curMappedDateTime.put(curDay, curTime);
+//            }
+//            // After finished the hashmap for the current lecture section, set the sliced info's
+//            // date time index to the hashmap, which is always index 1 according U of T format.
+//            lst.set(1, curMappedDateTime);
+//            mappedDateTime.add(lst);
+//        }
+//        return mappedDateTime;
+//    }
+
+
+
+//    /**
+//     * Returns a list of lectures, given a course.
+//     * PRECONDITION:     courseInfo is a list returned by getCourseInfo.
+//     * @param courseCode the courseCode, on the U of T coursefinder site.
+//     * @return           a list of lecture objects given a course.
+//     */
+//    private static List<Lecture> instantiateLectures(String courseCode) throws  IOException{
+//        List<DailyClass> dailyClasses = instantiateDailyClasses(courseCode);
+//        List<Lecture> lectures = new ArrayList<>();
+//        for(DailyClass dailyClass: dailyClasses){
+//            // Initialize the lecture with the lecture string(LEC 5101) and the time object.
+//            // Can modify lecture to take in additional info, such as
+//            Lecture curLecture = new Lecture((String)dailyClass.get(0), (HashMap)lst.get(1));
+//            lectures.add(curLecture);
+//        }
+//        return lectures;
+//    }
+//
+//    /**
+//     * Instantiates a course, given a courseCode String.
+//     * @param courseCode The course to instantiate.
+//     * @return           A course containing a course code and list of Lectures.
+//     */
+//    public static Course instantiateCourse(String courseCode) throws IOException {
+//        List<String> courseInfo = textScraper.getCourseInfo(courseCode);
+//        List<Lecture> lectures = instantiateLectures(courseInfo);
+//        Course newCourse = new Course(courseCode, lectures);
+//        return newCourse;
+//    }
 }
